8df4822dfce5ebff503bdd471363f9343135d3f8
Shashank singh <shashank.singh@binmile.com>
2024-09-16 11:25:55 +0530

    some changes

--- src/Utils/data/BeansData.ts
+++ src/Utils/data/BeansData.ts
@@ -4,8 +4,8 @@ const BeansData = [
     name: 'Robusta Beans',
     description: `Robusta beans are larger and more rounded than the other bean varieties. These plants typically grow much larger than Arabica plants, measuring between 15 and 20 feet. Robusta beans are typically considered to be hardier because they can grow at lower altitudes and resist diseases. But recent research suggests that they don’t handle heat as well as was previously thought.`,
     roasted: 'Medium Roasted',
-    imagelink_square: require('../assets/coffee_assets/robusta_coffee_beans/robusta_coffee_beans_square.png'),
-    imagelink_portrait: require('../assets/coffee_assets/robusta_coffee_beans/robusta_coffee_beans_portrait.png'),
+    imagelink_square: require('../../../assets/coffee_assets/robusta_coffee_beans/robusta_coffee_beans_square.png'),
+    imagelink_portrait: require('../../../assets/coffee_assets/robusta_coffee_beans/robusta_coffee_beans_portrait.png'),
     ingredients: 'Africa',
     special_ingredient: 'From Africa',
     prices: [
@@ -24,8 +24,8 @@ const BeansData = [
     name: 'Arabica Beans',
     description: `Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! The name Arabica likely comes from the beans’ popularity in 7th-century Arabia (present-day Yemen).`,
     roasted: 'Medium Roasted',
-    imagelink_square: require('../assets/coffee_assets/arabica_coffee_beans/arabica_coffee_beans_square.png'),
-    imagelink_portrait: require('../assets/coffee_assets/arabica_coffee_beans/arabica_coffee_beans_portrait.png'),
+    imagelink_square: require('../../../assets/coffee_assets/arabica_coffee_beans/arabica_coffee_beans_square.png'),
+    imagelink_portrait: require('../../../assets/coffee_assets/arabica_coffee_beans/arabica_coffee_beans_portrait.png'),
     ingredients: 'Africa',
     special_ingredient: 'From Africa',
     prices: [
@@ -44,8 +44,8 @@ const BeansData = [
     name: 'Liberica Beans',
     description: `Native to central and western Africa – specifically Liberia, hence its name – Coffea liberica is prized for its piquant floral aroma and bold, smoky flavor profile. Growing from a much larger plant than Arabica or Robusta, most Liberica cherries tend to be irregular in shape and closer to Robusta in size and general appearance. It’s also tolerant of hot, humid climates and does well at low altitude. `,
     roasted: 'Medium Roasted',
-    imagelink_square: require('../assets/coffee_assets/liberica_coffee_beans/liberica_coffee_beans_square.png'),
-    imagelink_portrait: require('../assets/coffee_assets/liberica_coffee_beans/liberica_coffee_beans_portrait.png'),
+    imagelink_square: require('../../../assets/coffee_assets/liberica_coffee_beans/liberica_coffee_beans_square.png'),
+    imagelink_portrait: require('../../../assets/coffee_assets/liberica_coffee_beans/liberica_coffee_beans_portrait.png'),
     ingredients: 'Malaysia',
     special_ingredient: 'From Malaysia',
     prices: [
@@ -64,8 +64,8 @@ const BeansData = [
     name: 'Excelsa Beans',
     description: `Excelsa beans grow almost entirely in Southeast Asia, and they’re shaped somewhat like Liberica beans — elongated ovals. These beans grow on large 20 to 30-foot coffee plants at medium altitudes. In terms of flavor, Excelsa beans are pretty unique. They combine light roast traits like tart notes and fruity flavors with flavors that are more reminiscent of dark roasts.`,
     roasted: 'Medium Roasted',
-    imagelink_square: require('../assets/coffee_assets/excelsa_coffee_beans/excelsa_coffee_beans_square.png'),
-    imagelink_portrait: require('../assets/coffee_assets/excelsa_coffee_beans/excelsa_coffee_beans_portrait.png'),
+    imagelink_square: require('../../../assets/coffee_assets/excelsa_coffee_beans/excelsa_coffee_beans_square.png'),
+    imagelink_portrait: require('../../../assets/coffee_assets/excelsa_coffee_beans/excelsa_coffee_beans_portrait.png'),
     ingredients: 'Malaysia',
     special_ingredient: 'From Malaysia',
     prices: [
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Poppins_500Medium } from '@expo-google-fonts/poppins'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function HomeScreen() {
 
@@ -11,6 +13,7 @@ export default function HomeScreen() {
   
   return (
     <View style={styles.container}>
+      <Ionicons name="heart-circle" size={32} color="green" />
       <Text style={styles.text}>Home Screen</Text>
     </View>
   );
