3838967cd5e2374b92ab2649319a5cd6d48094c8
dobliyal <rohit.dobliyal@binmile.com>
2024-09-16 12:44:13 +0530

    check

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Poppins_500Medium } from '@expo-google-fonts/poppins'
-import CustomIcon from '@/src/components/CustomIcon';
+// import CustomIcon from '@/src/components/CustomIcon';
 
 
 export default function ExploreScreen() {
@@ -14,7 +14,6 @@ export default function ExploreScreen() {
   return (
     
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , backgroundColor: '#0C0F14'}}>
-      <CustomIcon name="like" size={30} color="#fff" />
       <Text style={{color:'#fff', fontFamily:'Poppins', fontSize:30}}>Explore Screen</Text>
       
     </View>
